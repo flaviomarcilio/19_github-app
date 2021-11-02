dd5b15354dfeb6e0b1bfce28946a8be252e2f8de
flaviomarcilio <flaviomarciliodeoliveira@gmail.com>
2021-11-02 18:32:45 -0300

    Create GlobalStyle

---
+++ src/components/globalStyle/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: #040D21;
+        padding: 0;
+        margin: 0;
+        font-family: 'Roboto', sans-serif;
+        color: #fff;
+    }
+`;
